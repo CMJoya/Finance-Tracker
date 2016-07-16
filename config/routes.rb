b81a499f9de6ac4873b89cb72d6fb151c9f89b32
Cris Joya <MJoya@mac.attlocal.net>
2016-07-16 14:17:47 -0500

    Add Devise And BootStrap For Authentication And Views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Rails.application.routes.draw do
 
+  devise_for :users
   root 'welcome#index'
+
+  # get 'signup' => 'users#new'
+  # resources :users, except: [:new]
+  # get 'users' => 'users#index', as: :users
+  # get 'users/:id' => 'users#show', as: :users
+  # get 'users/:id/edit' => 'users#edit', as: :edit_user
+  # post 'users' => 'users#create'
+  # patch 'users/:id' => 'users#update'
+  # put 'users/:id' => 'users#update'
+  # delete 'users/:id' => 'users#delete'
+  #
+  # get 'login' => 'sessions#new'
+  # post 'login' => 'sessions#create'
+  # delete 'login' => 'sessions#destroy'
 end
