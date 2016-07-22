b545b626f66686ecd94869613eee7ae9a34e6a12
Cris Joya <MJoya@mac.attlocal.net>
2016-07-22 10:57:40 -0500

    Clean Up

--- config/routes.rb
+++ config/routes.rb
@@ -4,19 +4,4 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio' => 'users#my_portfolio'
   get 'search_stocks' => 'stocks#search'
-
-
-  # get 'signup' => 'users#new'
-  # resources :users, except: [:new]
-  # get 'users' => 'users#index', as: :users
-  # get 'users/:id' => 'users#show', as: :users
-  # get 'users/:id/edit' => 'users#edit', as: :edit_user
-  # post 'users' => 'users#create'
-  # patch 'users/:id' => 'users#update'
-  # put 'users/:id' => 'users#update'
-  # delete 'users/:id' => 'users#delete'
-  #
-  # get 'login' => 'sessions#new'
-  # post 'login' => 'sessions#create'
-  # delete 'login' => 'sessions#destroy'
 end
